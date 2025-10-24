bc8b3373905ab24a9bc07146bacc489c616254b8
Matthew Cole <matthewrichardcole@gmail.com>
2025-10-24 13:47:22 -0600

    add some hopefully useful utils for later

--- src/utils.h
+++ src/utils.h
@@ -10,6 +10,9 @@
   ((byte) & 0x04 ? '1' : '0'), \
   ((byte) & 0x02 ? '1' : '0'), \
   ((byte) & 0x01 ? '1' : '0') 
+
+#define BOOL_TO_LIT(val) (val) ? "true" : "false"
+#define JSON_IS_NULL(json_variant) ((json_variant).is<JsonVariant>() && (json_variant).isNull())
 // Just a Clamp value uitlity
 
 template <typename T> T CLAMP(const T& value, const T& low, const T& high) 
